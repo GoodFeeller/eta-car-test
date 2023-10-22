2bd367015bf09a068fa82acb2b7847bb03942de8
GoodFeeller <maksim.vasilenok@gmail.com>
2023-10-22 17:17:26 +0300

    Update CoinItem.tsx

--- src/components/pages/main-page/coin-table/coin-item/CoinItem.tsx
+++ src/components/pages/main-page/coin-table/coin-item/CoinItem.tsx
@@ -12,7 +12,7 @@ const CoinItem: FunctionComponent<{item: ICoin}> = ({item}) => {
             <tr onClick={() => navigate(`/coin/${item.id}`)}>
                 <td>{item.rank}</td>
                 <td className={styles.imgTd}>
-                    <img src={`https://assets.coincap.io/assets/icons/${item.symbol.toLowerCase()}@2x.png`}/>
+                    <img src={`https://assets.coincap.io/assets/icons/${item.symbol.toLowerCase()}@2x.png`} alt={'Not found'}/>
                     {item.symbol}
                 </td>
                 <td>{Intl.NumberFormat("en", { style: 'currency' , currency: "USD"}).format( Number(item.priceUsd) )}</td>
