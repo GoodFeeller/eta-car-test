b26c7c45d553a06d7a796aedc5f06859b856e12d
GoodFeeller <maksim.vasilenok@gmail.com>
2023-10-26 22:30:36 +0300

    change location

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import MainPage from './components/pages/main-page/MainPage'
-import CoinPage from './components/pages/coin-page/CoinPage'
+import MainPage from './pages/main-page/MainPage'
+import CoinPage from './pages/coin-page/CoinPage'
 import AddProvider from './providers/AddContext'
 import ProfileProvider from './providers/ProfileContext'
 
